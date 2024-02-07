08ce768a16b9a1cff41389385e089a887e017079
averpovskii <averpovskii@phoenixit.ru>
2024-02-07 18:46:22 +0300

    final commit

--- MyTest/ViewController.swift
+++ MyTest/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     private lazy var yesButton: CustomButton = {
         let yesButton = CustomButton()
         yesButton.setTitle("Да", for: .normal)
